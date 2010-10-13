1c142335e48593362f259eafab48b62c913ce31d
Dale Thatcher <git@dalethatcher.com>
2010-10-13 10:54:02 +0100

    Add swank dev dependency.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject blockpuzzle "1.0.0-SNAPSHOT"
   :description "FIXME: write"
+  :dev-dependencies [[swank-clojure "1.2.1"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :main puzzle)
